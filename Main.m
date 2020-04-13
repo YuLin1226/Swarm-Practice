727d9adbe19e6d14c9b2823148693aa604032323
YuLin <r08522622@ntu.edu.tw>
2020-04-13 18:04:16 +0800

    First Commit

---
+++ Main.m
@@ -0,0 +1,22 @@
+clear;
+clc;
+
+%% Parameters
+time_interval = [0 50];         % 計算時間
+ICs = [0,0,5,0]';                   % 4個初始條件: x, dx, y, dy
+circle = [5, 5, 3];
+
+
+%% ODE45
+[t,Robot_1] = ode45(@MobileRobotDynamics, time_interval, ICs);
+[t,Robot_2] = ode45(@MobileRobotDynamics, time_interval, [7, 0, 7, 0]);
+
+%% Result Plotting
+PlotCircle(circle(1), circle(2), circle(3));
+axis([0, 10, 0, 10]);
+grid on; hold on;
+for i = 1 : length(Robot_1)
+plot(Robot_1(i,1), Robot_1(i,3), 'b.');
+plot(Robot_2(i,1), Robot_2(i,3), 'b.');
+pause(0.01);
+end
---
+++ MobileRobotDynamics.m
@@ -0,0 +1,18 @@
+function dq = MobileRobotDynamics(t, q)
+M = 1;
+B = 1;
+kd = 9;
+ksk = 1;
+circle = [5, 5, 3];
+
+qt = [circle(1), circle(2)];
+R = circle(3);
+
+f  = [ -ksk * ( q(1) - qt(1) ) * ( (q(1)-qt(1))^2 + (q(3)-qt(2))^2 - R^2 ) ;
+       -ksk * ( q(3) - qt(2) ) * ( (q(1)-qt(1))^2 + (q(3)-qt(2))^2 - R^2 )];
+
+dq = [ q(2);
+       f(1)/M - kd/M*q(2) - B/M*q(2);
+       q(4);
+       f(2)/M - kd/M*q(4) - B/M*q(4)];
+end
